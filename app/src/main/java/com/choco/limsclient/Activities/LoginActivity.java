11383607ef11812abcf8d037869553197892fade
ChocoCLv <choco_cl@163.com>
2017-04-03 23:26:33 +0800

    添加动态申请相机权限

--- app/src/main/java/com/choco/limsclient/Activities/LoginActivity.java
+++ app/src/main/java/com/choco/limsclient/Activities/LoginActivity.java
@@ -2,7 +2,6 @@ package com.choco.limsclient.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,13 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.choco.limsclient.Activities.Student.MainActivity;
+import com.choco.limsclient.CommModule.CommThread;
 import com.choco.limsclient.R;
-import com.choco.limsclient.CommModule.*;
 import com.choco.limsclient.Util.Global;
 
-import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.util.HashMap;
@@ -47,26 +45,26 @@ public class LoginActivity extends Activity {
             }
         });
 
-        handler = new Handler(){
+        handler = new Handler() {
             @Override
-            public void handleMessage(Message msg){
-                if(msg.what== Global.FROM_COMMTHREAD){
+            public void handleMessage(Message msg) {
+                if (msg.what == Global.FROM_COMMTHREAD) {
                     try {
                         JSONTokener jsonParser = new JSONTokener(msg.obj.toString());
-                        JSONObject resp =  (JSONObject)jsonParser.nextValue();
+                        JSONObject resp = (JSONObject) jsonParser.nextValue();
                         String loginResult = resp.getString("LOGIN_STATUS");
-                        if(loginResult.equals("SUCCESS")){
-                            Log.i("login","登录成功");
+                        if (loginResult.equals("SUCCESS")) {
+                            Log.i("login", "登录成功");
                             String userType = resp.getString("USERTYPE");
                             startNewActivity(userType);
-                        }else if(loginResult.equals("FAILED")){
-                            Log.i("login","登录失败");
-                            Toast.makeText(LoginActivity.this,"Login failed.Please check your username and password",Toast.LENGTH_LONG)
+                        } else if (loginResult.equals("FAILED")) {
+                            Log.i("login", "登录失败");
+                            Toast.makeText(LoginActivity.this, "Login failed.Please check your username and password", Toast.LENGTH_LONG)
                                     .show();
                         }
-                    }catch (JSONException e){
+                    } catch (JSONException e) {
                         e.printStackTrace();
-                        Log.i("login","illegal json data");
+                        Log.i("login", "illegal json data");
                     }
                 }
             }
@@ -75,41 +73,41 @@ public class LoginActivity extends Activity {
         new Thread(comm).start();
     }
 
-    private void startNewActivity(String userType){
-        HashMap<String,String> userTypeToActivity = new HashMap<>();
-        userTypeToActivity.put("STUDENT","com.choco.limsclient.Activities.Student.MainActivity");
-        userTypeToActivity.put("TEACHER","com.choco.limsclient.Activities.Teacher.MainActivity");
-        try{
-            Intent intent = new Intent(LoginActivity.this,Class.forName(userTypeToActivity.get(userType)));
+    private void startNewActivity(String userType) {
+        HashMap<String, String> userTypeToActivity = new HashMap<>();
+        userTypeToActivity.put("STUDENT", "com.choco.limsclient.Activities.Student.MainActivity");
+        userTypeToActivity.put("TEACHER", "com.choco.limsclient.Activities.Teacher.MainActivity");
+        try {
+            Intent intent = new Intent(LoginActivity.this, Class.forName(userTypeToActivity.get(userType)));
             startActivity(intent);
-        }catch (ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private void login(){
+    private void login() {
 
-        JSONObject jo =  new JSONObject();
+        JSONObject jo = new JSONObject();
         try {
             jo.put("USERNAME", edtTxtUsername.getText().toString());
             jo.put("PASSWORD", edtTxtPwd.getText().toString());
-        }catch (org.json.JSONException e){
+        } catch (org.json.JSONException e) {
             e.printStackTrace();
         }
 
         Message msg = new Message();
         msg.what = Global.FROM_UITHREAD;
         msg.obj = jo.toString();
-        Log.i("login",jo.toString());
+        Log.i("login", jo.toString());
         comm.rcvHandler.sendMessage(msg);
-        Log.i("login","send msg to comm thread");
+        Log.i("login", "send msg to comm thread");
 
     }
 
-    private  void findView(){
-        edtTxtUsername = (EditText)findViewById(R.id.edtTxt_userName);
-        edtTxtPwd=(EditText)findViewById(R.id.edtText_pwd);
-        btnLogin=(Button)findViewById(R.id.btn_login);
+    private void findView() {
+        edtTxtUsername = (EditText) findViewById(R.id.edtTxt_userName);
+        edtTxtPwd = (EditText) findViewById(R.id.edtText_pwd);
+        btnLogin = (Button) findViewById(R.id.btn_login);
 
         edtTxtUsername.setText("2013010918015");
         edtTxtPwd.setText("8682502101");
--- app/src/main/java/com/choco/limsclient/Activities/Student/MainActivity.java
+++ app/src/main/java/com/choco/limsclient/Activities/Student/MainActivity.java
@@ -2,21 +2,27 @@ package com.choco.limsclient.Activities.Student;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.choco.limsclient.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
-import com.choco.limsclient.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
     Button btnBorrowDevice;
+    String permissions[] = {"android.permission.CAMERA"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +33,23 @@ public class MainActivity extends Activity {
         btnBorrowDevice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                checkPermissions();
                 borrowDevice();
             }
         });
 
     }
 
-    private void findView(){
-        btnBorrowDevice = (Button)findViewById(R.id.btn_borrowDevice);
+    private void findView() {
+        btnBorrowDevice = (Button) findViewById(R.id.btn_borrowDevice);
     }
 
-    private void borrowDevice(){
-        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+    private void borrowDevice() {
+        IntentIntegrator integrator = new IntentIntegrator(com.choco.limsclient.Activities.Student.MainActivity.this);
         ArrayList<String> formats = new ArrayList<>();
         formats.add("QR_CODE");
         integrator.setPrompt("test");
-        integrator.setScanningRectangle(600,600);
+        integrator.setScanningRectangle(600, 600);
         integrator.initiateScan(formats);
     }
 
@@ -52,9 +59,23 @@ public class MainActivity extends Activity {
         if (scanResult != null) {
             String result = scanResult.getContents();
             Log.d("code", result);
-            Toast.makeText(this,result, Toast.LENGTH_LONG).show();
-        }else{
-            super.onActivityResult(requestCode,resultCode,data);
+            Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : permissions) {
+                // 检查该权限是否已经获取
+                int i = ContextCompat.checkSelfPermission(this, permission);
+                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 如果没有授予该权限，就去提示用户请求
+                    ActivityCompat.requestPermissions(this, permissions, 321);
+                }
+            }
         }
     }
 }
