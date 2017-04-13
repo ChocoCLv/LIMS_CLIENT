49421453d352a75faf317b2c2653e4ff4f723ce8
ChocoCLv <choco_cl@163.com>
2017-04-13 20:38:48 +0800

    添加学生用户借入设备界面（之前是在主界面点击后即扫描二维码）

--- app/src/main/java/com/choco/limsclient/Activities/Student/MainActivity.java
+++ app/src/main/java/com/choco/limsclient/Activities/Student/MainActivity.java
@@ -1,26 +1,17 @@
 package com.choco.limsclient.Activities.Student;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.choco.limsclient.Activities.QRCode.ScanActivity;
 import com.choco.limsclient.R;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import static com.choco.limsclient.Config.Global.userInfo;
 
 public class MainActivity extends AppCompatActivity {
     Button btnBorrowDevice;
-    String permissions[] = {"android.permission.CAMERA"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,74 +23,13 @@ public class MainActivity extends AppCompatActivity {
         btnBorrowDevice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (checkPermissions()) {
-                    borrowDevice();
-                }
+                Intent intent = new Intent(com.choco.limsclient.Activities.Student.MainActivity.this, BorrowDeviceActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 
     private void findView() {
         btnBorrowDevice = (Button) findViewById(R.id.btn_borrowDevice);
-    }
-
-    private void borrowDevice() {
-        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-        intentIntegrator.setCaptureActivity(ScanActivity.class);
-        intentIntegrator.setOrientationLocked(false);
-        intentIntegrator.initiateScan();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private boolean checkPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (String permission : permissions) {
-                // 检查该权限是否已经获取
-                int i = ContextCompat.checkSelfPermission(this, permission);
-                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
-                if (i != PackageManager.PERMISSION_GRANTED) {
-                    // 如果没有授予该权限，就去提示用户请求  异步方法 调用后即返回
-                    ActivityCompat.requestPermissions(this, permissions, 321);
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 321: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    borrowDevice();
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
     }
 }
